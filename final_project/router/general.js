483cf4a6ad96022a582eb644c4f73b8aa8662baf
jansjay <sanjaya.jayasooriya@gmail.com>
2023-03-07 18:01:02 -0500

    Tasks Promise

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -20,42 +20,77 @@ public_users.post("/register", (req,res) => {
 });
 
 public_users.get('/',function (req, res) {
-    res.send(JSON.stringify(books, null, 3));
+    let responsePromise = new Promise((resolve,reject) => {
+        setTimeout(() => {
+            resolve(JSON.stringify(books, null, 3));
+        });
+    });
+    responsePromise.then((response) => {
+        res.send(response);
+    });
 });
 
 public_users.get('/isbn/:isbn',function (req, res) {
-    const isbn = req.params.isbn;
-    res.send(JSON.stringify(books[isbn], null, 3));
- });
+    let responsePromise = new Promise((resolve,reject) => {
+        setTimeout(() => {
+            const isbn = req.params.isbn;
+            resolve(JSON.stringify(books[isbn], null, 3));
+        });
+    });
+    responsePromise.then((response) => {
+        res.send(response);
+    });
+});
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-    const author = req.params.author;
-    let booksArray = [];
-    for(var id in books){
-        if(books[id].author === author) {
-            booksArray.push(books[id]);
-        }
-    }
-    res.send(JSON.stringify(booksArray, null, 3))
+    let responsePromise = new Promise((resolve,reject) => {
+        setTimeout(() => {
+            const author = req.params.author;
+            let booksArray = [];
+            for(var id in books){
+                if(books[id].author === author) {
+                    booksArray.push(books[id]);
+                }
+            }
+            resolve(JSON.stringify(booksArray, null, 3));
+        });
+    });
+    responsePromise.then((response) => {
+        res.send(response);
+    });
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-    const title = req.params.title;
-    let booksArray = [];
-    for(var id in books){
-        if(books[id].title === title) {
-            booksArray.push(books[id]);
-        }
-    }
-    res.send(JSON.stringify(booksArray, null, 3))
+    let responsePromise = new Promise((resolve,reject) => {
+        setTimeout(() => {
+            const title = req.params.title;
+            let booksArray = [];
+            for(var id in books){
+                if(books[id].title === title) {
+                    booksArray.push(books[id]);
+                }
+            }
+            resolve(JSON.stringify(booksArray, null, 3));
+        });
+    });
+    responsePromise.then((response) => {
+        res.send(response);
+    });
 });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-    const isbn = req.params.isbn;
-    res.send(JSON.stringify(books[isbn].reviews));
+    let responsePromise = new Promise((resolve,reject) => {
+        setTimeout(() => {
+            const isbn = req.params.isbn;
+            res.send(JSON.stringify(books[isbn].reviews));
+        });
+    });
+    responsePromise.then((response) => {
+        res.send(response);
+    });
 });
 
 module.exports.general = public_users;
